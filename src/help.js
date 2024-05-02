89e873f1d03bc4407004c9ce50e3ee85e42ff0e3
OumaymaHadjYahia <133598920+OumaymaHadj@users.noreply.github.com>
2024-05-02 15:12:07 +0200

    Add files via upload

--- src/help.js
+++ src/help.js
@@ -1,55 +1,49 @@
+import inquirer from 'inquirer';
 import initializeProject from './createProject.js';
+import gatherDatabaseInfo from './databaseInfo.js'
+import createComponent from './createComponents.js';
+import sendMessageToChatGPT from './chatGPTReq.js';
 
-const showCommands = () => {
-    console.log(`
-  List of Commands:
-  
-  1. 'createProject':
-     Create a specific project.
-  
-     Example:
-     createProject 
-  
-  2. 'cofigureDB':
-     Configuration of your database.
-  
-     Example:
-     configureDB  nameOfProject
-  
-  3. 'connectDB':
-     Access to your database.
-  
-     Example:
-     connectDB nameOfProject
-    `);
-};
 
-// Parse command line arguments
-const command = process.argv[2];
 
-// Perform actions based on the command
-switch (command) {
-    case 'createProject':
-        console.log("Create a specific project...");
+export default function help() {
+  const commands = [
+    { name: 'Create a new project', value: 'createProject' },
+    { name: 'Establish database connection', value: 'connectDB' },
+    { name: 'Create Model, Service and Component for a spesific table', value: 'createComponent' },
+    { name: 'Send request to chatGpt', value: 'chatWithGPT' }
+  ];
+
+  // Function to execute the selected command
+  function executeCommand(command) {
+
+    switch (command) {
+      case 'createProject':
         initializeProject();
         break;
 
-    case 'cofigureDB':
-        console.log("Configuration of your database...");
-        // Implement logic to show tables here
+      case 'connectDB':
+        gatherDatabaseInfo();
         break;
 
-    case 'connectDB':
-        console.log("Access to your database...");
-        // Implement logic to extract fields and types here
+      case 'createComponent':
+        createComponent();
         break;
+      case 'chatWithGPT':
+        sendMessageToChatGPT();
+        break;
+        
+    }
 
-    case 'help':
-        showCommands();
-        break;
-
-    default:
-        console.log("Invalid command. Please use one of the following commands:");
-        showCommands();
-        break;
+  }
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'command',
+      message: 'Choose a command:',
+      choices: commands
+    }
+  ]).then(answers => {
+    executeCommand(answers.command);
+  });
 }
